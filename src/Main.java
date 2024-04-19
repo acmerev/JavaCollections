8218d146ca4a45340966ad61d1c5eb52dfa648a5
acmerev <acmerev@gmail.com>
2024-04-19 09:48:30 -0600

    test etream

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        List<String> estudiantes = List.of("Anel","ALberto","Briyan", "jose");
 
+        List<String> nombres =  estudiantes.stream()
+                .filter(nombre-> nombre.startsWith("A"))
+                .collect(Collectors.toList());
 
-        System.out.println("Hello world!");
+        for(String nom : nombres){
+            System.out.println("nombre: " +nom);
+        }
+
+        System.out.println(nombres  + "\n");
+
+        HashSet<String> hs = new HashSet<String>();
+
+        hs.add("For");
+        hs.add("Geeks");
+        hs.add("Is");
+        hs.add("Very helpful");
+
+            System.out.println(hs);
+
     }
 }
